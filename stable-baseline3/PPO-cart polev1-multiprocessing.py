7940dfc91e3c43567ef6ea045a8ca3f315264186
陈振江 <ilyxjj@iCloud.com>
2024-05-10 11:53:01 +0800

    Initial commit

---
+++ stable-baseline3/PPO-cart polev1-multiprocessing.py
@@ -0,0 +1,51 @@
+import gymnasium as gym
+
+from stable_baselines3 import PPO, SAC
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.utils import set_random_seed
+
+def make_env(env_id: str, rank: int, seed: int = 0):
+    """
+    Utility function for multiprocessed env.
+
+    :param env_id: the environment ID
+    :param num_env: the number of environments you wish to have in subprocesses
+    :param seed: the inital seed for RNG
+    :param rank: index of the subprocess
+    """
+    def _init():
+        env = gym.make(env_id, render_mode="human")
+        env.reset(seed=seed + rank)
+        return env
+    set_random_seed(seed)
+    return _init
+
+if __name__ == "__main__":
+    env_id = "BipedalWalker-v3"
+    num_cpu = 1  # Number of processes to use
+    # Create the vectorized environment
+    vec_env = SubprocVecEnv([make_env(env_id, i) for i in range(num_cpu)])
+
+    # Stable Baselines provides you with make_vec_env() helper
+    # which does exactly the previous steps for you.
+    # You can choose between `DummyVecEnv` (usually faster) and `SubprocVecEnv`
+    # env = make_vec_env(env_id, n_envs=num_cpu, seed=0, vec_env_cls=SubprocVecEnv)
+
+    model = PPO("MlpPolicy", vec_env, verbose=1)
+    print(model.policy)
+    print(model.policy.critic.qf0)
+    exit(0)
+    model.learn(total_timesteps=25000)
+
+    obs = vec_env.reset()
+    for _ in range(1000):
+        action, _states = model.predict(obs)
+        print(action)
+        print(type(action))
+        obs, rewards, dones, info = vec_env.step(action)
+        print(rewards)
+        print(type(rewards))
+        print(obs)
+        print(type(obs))
+        vec_env.render()
